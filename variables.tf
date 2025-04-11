39f529f0184821287a856017bceeb4768eb6ba80
K-Basavaraj <kuruvabasavaraj09@gmail.com>
2025-04-11 16:49:23 +0530

    updated

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {}
+
+variable "environment" {}
+
+variable "sg_name" {}
+
+variable "common_tags" {
+  default = {}
+}
+
+variable "sg_tags" {
+  default = {}
+}
+
+variable "vpc_id" {}
